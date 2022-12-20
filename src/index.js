148d64701a8dbdd26dcbe6b4b1bc6d3459198771
Christian Ani <iamchristianani@gmail.com>
2022-12-20 13:09:45 +0100

    Create JS file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./css/style.css";
+import { refreshBtn, formBtn, formName, formScore, scoreBox } from "./modules/variableList";
+import {renderDisplay} from "./modules/addScores";
+
+renderDisplay();
+
+console.log(refreshBtn);
+console.log(formBtn);
+console.log(formName);
+console.log(formScore);
+console.log(scoreBox);
