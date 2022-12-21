44fa7696c241a7e4696f78cb7c77759a3d2a9584
Christian Ani <iamchristianani@gmail.com>
2022-12-21 16:43:23 +0100

    Create function to create game API ID

---
+++ src/modules/createGame.js
@@ -0,0 +1,15 @@
+const createGame = () => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'Christians Game',
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+
+export default createGame;
